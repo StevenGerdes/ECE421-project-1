6f19b3025976f765967193d3472519313f92964b
unknown <meen@ualberta.ca>
2014-10-02 00:04:01 -0600

    Completed matrix_factory_contract and Abstract_matrix_factory_contract

--- Contracts/abstract_matrix_factory_contract.rb
+++ Contracts/abstract_matrix_factory_contract.rb
@@ -7,21 +7,44 @@ require '../sparse_matrix_factory'
 require '../tridiagonal_matrix_factory'
 
 class AbstractMatrixFactoryContract < Test::Unit::TestCase
-  def test_build
+  def test_build_block
     abstract_matrix_factory = AbstractMatrixFactory.new
+    matrix_row = Numeric.new
+    matrix_col = Numeric.new
+
     #Invarient
     old_matrix_factory = abstract_matrix_factory.clone
 
     #Pre-conditions
-    #None
+    assert_respond_to(matrix_row, :to_i)
+    assert_respond_to(matrix_col, :to_i)
 
-    new_abstract_matrix = abstract_matrix_factory.build(5, 5) {|row, col| col - row }
-    new_matrix = Matrix(5, 5) {|row, col| col - row }
+    new_abstract_matrix = abstract_matrix_factory.build(matrix_row, matrix_col) {|row, col| col - row }
 
     #Post-condition
-    new_matrix.instance_methods.each do |method|
-      assert_true(new_abstract_matrix.respond_to?(method))
+    assert_true(post_condition_test(new_abstract_matrix))
+
+    #Invarient
+    assert_equal(old_matrix_factory, abstract_matrix_factory)
+  end
+
+  def test_build_matrix
+    abstract_matrix_factory = AbstractMatrixFactory.new
+    input_array = Array.new
+
+    #Invarient
+    old_matrix_factory = abstract_matrix_factory.clone
+
+    #Pre-conditions
+    assert_kind_of(Array, input_array)
+    input_array.each do |entry|
+      assert_kind_of(Numeric, entry)
     end
+
+    new_abstract_matrix = abstract_matrix_factory.build(input_array)
+
+    #Post-condition
+    assert_true(post_condition_test(new_abstract_matrix))
 
     #Invarient
     assert_equal(old_matrix_factory, abstract_matrix_factory)
@@ -29,28 +52,68 @@ class AbstractMatrixFactoryContract < Test::Unit::TestCase
 
   def test_zero
     abstract_matrix_factory = AbstractMatrixFactory.new
+    matrix_arg = Numeric.new
+
     #Invarient
     old_matrix_factory = abstract_matrix_factory.clone
 
     #Pre-conditions
-    #None
+    assert_respond_to(matrix_arg, :to_i)
 
-    new_abstract_matrix = abstract_matrix_factory.zero(5)
-    new_matrix = Matrix.zero(5)
+    new_abstract_matrix = abstract_matrix_factory.zero(matrix_arg)
 
     #Post-condition
-    new_matrix.instance_methods.each do |method|
-      assert_true(new_abstract_matrix.respond_to?(method))
-    end
+    assert_true(post_condition_test(new_abstract_matrix))
+
+    #Invarient
+    assert_equal(old_matrix_factory, abstract_matrix_factory)
+  end
+
+  def test_identity
+    abstract_matrix_factory = AbstractMatrixFactory.new
+    matrix_arg = Numeric.new
+
+    #Invarient
+    old_matrix_factory = abstract_matrix_factory.clone
+
+    #Pre-conditions
+    assert_respond_to(matrix_arg, :to_i)
+
+    new_abstract_matrix = abstract_matrix_factory.identity(matrix_arg)
+
+    #Post-condition
+    assert_true(post_condition_test(new_abstract_matrix))
 
     #Invarient
     assert_equal(old_matrix_factory, abstract_matrix_factory)
   end
 
   def post_condition_test(new_abstract_matrix)
-    new_matrix = Matrix(5, 5) {|row, col| col - row }
-    new_matrix.instance_methods.each do |method|
-      assert_true(new_abstract_matrix.respond_to?(method))
+    new_methods = [:multiply!,
+                   :scalar_multiply,
+                   :scalar_multiply,
+                   :add!,
+                   :scalar_add!,
+                   :scalar_add,
+                   :subtract!,
+                   :scalar_subtract,
+                   :scalar_subtract!,
+                   :divide!,
+                   :scalar_divide,
+                   :scalar_divide!,
+                   :inverse!,
+                   :transpose!,
+                   :round!]
+    Matrix.instance_methods.each do |method|
+      unless new_abstract_matrix.respond_to?(method)
+        return false
+      end
+    end
+
+    new_methods.each do |method|
+      unless new_abstract_matrix.respond_to?(method)
+        return false
+      end
     end
   end
 end
--- Contracts/matrix_factory_contract.rb
+++ Contracts/matrix_factory_contract.rb
@@ -1,9 +1,82 @@
 gem 'test-unit'
 require 'test/unit'
 require '../matrix'
+require '../matrix_factory'
 require '../sparse_matrix'
+require '../sparse_matrix_factory'
 require '../tridiagonal_matrix'
+require '../tridiagonal_matrix_factory'
 
 class MatrixFactoryContract < Test::Unit::TestCase
+  def test_valid_data_sparse
+    input_matrix = Matrix.I(5)
 
+    #Invarient
+    old_matrix = input_matrix.clone
+
+    #Pre-conditions
+    assert_kind_of(Matrix, input_matrix)
+
+    result = SparseMatrixFactory.valid_data(input_matrix)
+
+    #Post-condition
+    assert_true(result.is_a?(TrueClass) or result.is_a?(FalseClass))
+
+    #Invarient
+    assert_equal(old_matrix, input_matrix)
+  end
+
+  def test_valid_data_tridiagonal
+    input_matrix = Matrix.I(5)
+
+    #Invarient
+    old_matrix = input_matrix.clone
+
+    #Pre-conditions
+    assert_kind_of(Matrix, input_matrix)
+
+    result = TridiagonalMatrixFactory.valid_data(input_matrix)
+
+    #Post-condition
+    assert_true(result.is_a?(TrueClass) or result.is_a?(FalseClass))
+
+    #Invarient
+    assert_equal(old_matrix, input_matrix)
+  end
+
+  def test_build_sparse
+    input_matrix = Matrix.I(5)
+
+    #Invarient
+    old_matrix = input_matrix.clone
+
+    #Pre-conditions
+    assert_kind_of(Matrix, input_matrix)
+
+    result = SparseMatrixFactory.build(input_matrix)
+
+    #Post-condition
+    assert_kind_of(SparseMatrix, result)
+
+    #Invarient
+    assert_equal(old_matrix, input_matrix)
+  end
+
+  def test_build_tridiagonal
+    input_matrix = Matrix.I(5)
+
+    #Invarient
+    old_matrix = input_matrix.clone
+
+    #Pre-conditions
+    assert_kind_of(Matrix, input_matrix)
+
+    result = TridiagonalMatrixFactory.build(input_matrix)
+
+    #Post-condition
+    assert_kind_of(TridiagonalMatrix, result)
+
+    #Invarient
+    assert_equal(old_matrix, input_matrix)
+  end
 end
