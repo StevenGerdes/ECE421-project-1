73059b3aaad6ebc140c9a82727b2de89c5ef4d09
unknown <meen@ualberta.ca>
2014-10-09 12:06:14 -0600

    Put examples into assignment1_main method

--- assignment1_main.rb
+++ assignment1_main.rb
@@ -1,17 +1,25 @@
+require './abstract_matrix_factory'
+require './sparse_matrix_factory'
+require './sparse_matrix'
 require './matrix'
 
-m = Matrix.new
-result = m.test_method
-m.out
-result.out
-puts "break"
-result = m.test_method_with_args(1,2)
-m.out
-result.out
-puts "break"
+#this code is run using one of the build methods in the abstract matrix factory
+#when a matrix is built, it will either return a normal matrix or a sparse matrix
+#depending on the input. if there are less then 50% of the values filled in then
+#it will be a sparse matrix. the user will not know which matrix they have gotten
+#back. all functions described in the matrix_contract file function on both kinds
+#of matrixes.
 
-m = Matrix.new
-m.test_method!
-m.out
-m.test_method_with_args!(2,3)
-m.out
+#create a matrix using the AbstractMatrixFactory
+sparse_matrix = AbstractMatrixFactory.I(5)
+normal_matrix = AbstractMatrixFactory.[]([1, 2, 3, 4, 5], [6, 7, 8, 9, 0], [5, 4, 3, 2, 1])
+
+#perform actions on them, actions work on both matrixes
+sparse_matrix.scalar_multiply!(5)
+normal_matrix.scalar_multiply!(5)
+
+new_matrix = sparse_matrix + normal_matrix
+
+inverse_matrix = new_matrix.inverse()
+
+eigen_value = new_matrix.eigen
