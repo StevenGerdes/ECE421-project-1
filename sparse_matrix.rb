69aaf48b807805451219e2bab6985195f80c782f
unknown <meen@ualberta.ca>
2014-10-09 11:16:29 -0600

    Finished enterign comments

--- sparse_matrix.rb
+++ sparse_matrix.rb
@@ -77,7 +77,7 @@ class SparseMatrix
     call_method_immutably(__method__, i)
   end
 
-  #reutnrs resulf of multiplying a matrix by its inverse
+  #reutnrs result of multiplying a matrix by its inverse
   def /(matrix)
     to_matrix / matrix
   end
@@ -94,196 +94,241 @@ class SparseMatrix
     end
   end
 
-
+  #return matrix after each method has been divided by a a scalar
   def scalar_divide(i)
     call_method_immutably(__method__, i)
   end
 
+  #Add matrix to the sparse matrix
   def +(matrix)
     to_matrix + matrix
   end
 
+  #add matrix to current sparse matrix, and set current sparse matrix to be result
   def add!(matrix)
     to_sparse!(to_matrix + matrix)
   end
 
+  #add a scalar to itself
   def scalar_add!(i)
     to_sparse!(to_matrix.scalar_add(i))
   end
 
+  #add a scalar to itself but only return result
   def scalar_add(i)
     call_method_immutably(__method__, i)
   end
 
+  #subtract matrix from sparse matrix
   def -(matrix)
     to_matrix - matrix
   end
 
+  #subtract matrix from sparse matrix and set sparse matrix to result
   def subtract!(matrix)
     to_sparse!(to_matrix - matrix)
   end
 
+  #subtract scalar from sparse matrix and change sparse matrix to result
   def scalar_subtract!(i)
     to_sparse!(to_matrix.scalar_subtract(i))
   end
 
+  #subtract scalar from sparse matrix and return result
   def scalar_subtract(i)
     call_method_immutably(__method__, i)
   end
 
-  def conjugate
-    to_matrix.conjugate
+  #set matrix to conjugate matrix
+  def conjugate!
+    to_sparse!(to_matrix.conjugate)
   end
 
-  def conjugate!
+  #return conjugate matrix
+  def conjugate
     call_method_immutably(__method__, i)
   end
 
-  def determinant()
+  #returns determinant
+  def determinant
     to_matrix.determinant
   end
 
+  #returns the eigen values
   def eigen
     to_matrix.eigensystem
   end
 
+  #calculates imaginary
   def imaginary
     to_matrix.imaginary
   end
 
+  #calculates sets matrix to its inverse
   def inverse!()
-    to_sparse!(to_matrix.inverse())
+    to_sparse!(to_matrix.inverse)
   end
 
+  #returns matrix inverse
   def inverse()
     call_method_immutably(__method__, i)
   end
 
+  #performs lup decomposision on the matrix
   def lup_decomposition
     to_matrix.lup_decomposition
   end
 
+  #returns rank
   def rank
     to_matrix.rank
   end
 
+  #returns real
   def real
     to_matrix.real
   end
 
+  #returns rectangular
   def rectangular
     to_matrix.rectangular
   end
 
+  #resizes matrix to i, j
   def resize!(i, j)
     @row = i
     @col = j
   end
 
+  #sets matrix to rounded number
   def round!(i)
     @elements.each_value { |value|
       value.round(i) }
   end
 
+  #returns rounded matrix
   def round(i)
     call_method_immutably(__method__, i)
   end
 
+  #returns trace
   def trace
     to_matrix.trace
   end
 
-  def transpose!()
-    to_sparse!(to_matrix.transpose())
+  #sets matrix to its transpose
+  def transpose!
+    to_sparse!(to_matrix.transpose)
   end
 
-  def transpose()
+  #returns matrixes transpose
+  def transpose
     call_method_immutably(__method__, i)
   end
 
+  #returns matrix as an array
   def to_a
     to_matrix.to_a
   end
 
+  #returns matrix as string
   def to_s
     to_matrix.to_s
   end
 
+  #returns if diagonal
   def diagonal?
     to_matrix.diagonal?
   end
 
+  #returns if hermitatian
   def hermitian?
     to_matrix.hermitian?
   end
 
+  #returns if normal
   def normal?
     to_matrix.normal?
   end
 
+  #returns if orthogonal
   def orthogonal?
     to_matrix.orthogonal?
   end
 
+  #returns if permutation
   def permutation?
     to_matrix.permutation?
   end
 
+  #returns ifsymmetric
   def symmetric?
     to_matrix.symmetric?
   end
 
+  #returns if unitary
   def unitary?
     to_matrix.unitary?
   end
 
+  #returns if lower triangular
   def lower_triangular?
     to_matrix.lower_triangular?
   end
 
+  #returns if real
   def real?
     to_matrix.real?
   end
 
+  #returns if regular
   def regular?
     to_matrix.regular?
   end
 
+  #returns if singular
   def singular?
     to_matrix.singular?
   end
 
+  #returns if square
   def square?
     to_matrix.square?
   end
 
+  #returns if upper triangular
   def upper_triangular?
     to_matrix.upper_triangular?
   end
 
+  #returns if zero
   def zero?
     to_matrix.zero?
   end
 
   private
+  #returns posistion as element index
   def index(i, j)
     (i)*@row + (j)
   end
 
+  #returns element index as posistion
   def position(i)
     return i % @col, (i / @row) + 1
   end
 
+  #returns matrix representation
   def to_matrix
-    returnMatrix = Matrix.zero(@row, @col)
+    return_matrix = Matrix.zero(@row, @col)
     @elements.each_pair do |key, value|
       i, j = position(key)
-      returnMatrix = returnMatrix.set(i, j, value)
+      return_matrix = return_matrix.set(i, j, value)
     end
 
-    return returnMatrix
+    return_matrix
   end
 
+  #turns matrix into a sparse matrix representations and sets results
   def to_sparse!(matrix)
     @row = matrix.row_size
     @col = matrix.column_size
